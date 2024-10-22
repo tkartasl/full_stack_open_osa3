4b4016896b8ab6862022e19521fa36c7067db8ce
Tomi Kartaslammi <tomi.kartaslammi@gmail.com>
2024-10-22 17:15:03 +0300

    Finished first exercises

---
+++ index.js
@@ -0,0 +1,93 @@
+const express = require('express')
+const app = express()
+
+let persons = [
+  {
+    id: "1",
+    name: "Artp Hellas",
+    number: "040-123456"
+  },
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523"
+  },
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345"
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122"
+  }
+]
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server is running on port ${PORT}`)
+app.use(express.json())
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+  const contactCount = persons.length
+  const now = Date()
+  const info = `Phonebook has info for ${contactCount} people`
+
+  response.setHeader('Content-Type', 'text/plain')
+  response.send(info + '\n' + now)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  const person = persons.find(person => person.id === id)
+  
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
+
+const generateId = () => {
+  return String(Math.floor(Math.random() * 1000000))
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  
+  console.log(body)
+  if (!body.name) {
+    return response.status(400).json({ 
+      error: 'name missing' 
+    })
+  }
+  if (!body.number) {
+    return response.status(400).json({ 
+      error: 'number missing' 
+    })
+  }
+  if (persons.find(person => person.name === body.name)) {
+    return response.status(400).json({ 
+      error: 'name must be unique' 
+    })
+  }
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  }
+  persons = persons.concat(person)
+  response.json(person)
+})
